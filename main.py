024fd611610ba413bf8240e45ff10f5ad29c2372
Felixatika <43841984+Felixatika@users.noreply.github.com>
2023-06-12 22:08:47 +0000

    Added More strings

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
-#print("Hello World!!!!!!!!!!!!!!!")
-#print('I am Iron man')
+print("Hello World!!!!!!!!!!!!!!!")
+print('I am Iron man')
 #We are practicing python
 print('I am Iron Man \n' + 'No, I am Tony Stark \n' + 'No, I am poppy ')
 print("I am poppy \n" * 100)
